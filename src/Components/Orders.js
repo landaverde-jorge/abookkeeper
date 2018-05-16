bce7565462163cb52c9d518a154e4d2168dc1cdf
Jorge Landaverde <jorgelandaverde@jorges-mbp.elcamino.edu>
2018-05-16 14:19:10 -0700

    starting new react project

---
+++ src/Components/Orders.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Orders extends Component {
+  render() {
+    return (
+      <div>
+        Hello from orders
+      </div>
+    );
+  }
+}
+
+export default Orders;
---
+++ src/Components/Periods.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Periods extends Component {
+  render() {
+    return (
+      <div >
+        Hello from Periods
+      </div>
+    );
+  }
+}
+
+export default Periods;
